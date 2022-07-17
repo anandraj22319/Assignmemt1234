58fff43473ac97353779a0b05456640e60644a5f
anandraj22319 <109364899+anandraj22319@users.noreply.github.com>
2022-07-17 11:51:59 +0530

    Add files via upload

---
+++ A4Q10.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i,x;
+    for(i=1;i<=10;i++)
+    {
+        x=5*i;
+        printf("%d ",x);
+    }
+    return 0;
+}
---
+++ A4Q8.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i,a;
+    for(i=1;i<=10;i++)
+    {
+        a=i*i;
+        printf("%d ",a);
+    }
+    return 0;
+}
